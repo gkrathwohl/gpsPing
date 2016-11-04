1047e608135855ba30d27c53c548683f599438fd
Greg Krathwohl <gkrathwohl@gmail.com>
2016-11-03 20:11:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express()
+
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('pingDB.db')
+
+
+app.get('/setup', function (req, res) {
+  
+  db.run('CREATE TABLE pings (time DATETIME)')
+  res.send('setup!')
+})
+
+app.get('/ping', function (req, res) {
+
+  db.serialize(function () {
+    
+    
+    db.run("INSERT INTO pings VALUES (datetime('now'))")
+
+    db.all('SELECT time FROM pings', function (err, row) {
+      console.log(row)
+    })
+  })
+
+  //db.close()
+
+  res.send('ping!')
+
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
